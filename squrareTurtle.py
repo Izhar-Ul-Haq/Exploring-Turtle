063ed43812fd66feaa5cccf3e82ce7a106f9e9db
Izhar-Ul-Haq <iizharulhaq9@gmail.com>
2023-05-17 12:21:13 +0500

    Square in Turtle

---
+++ squrareTurtle.py
@@ -0,0 +1,13 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("blue")
+wn.title("Izhar Ul Haq Learning Turtle")
+
+
+skk = turtle.Turtle()
+
+for i in range(4):
+    skk.forward(100)
+    skk.right(90)
+
+turtle.done()
